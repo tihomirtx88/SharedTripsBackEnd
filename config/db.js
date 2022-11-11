9ff7df4668e1acef22ad8570b2479bd41a3a4798
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2022-11-11 15:30:36 +0200

    delete package-lock.json

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mongoose = require(`mongoose`);
 require(`../models/User`);
 require(`../models/Trip`);
 
-//Todo change database name 
 const dababaseName = `sharedTrip`
-const conetctionString = `mongodb://localhost:27017/${dababaseName}`
+// const conetctionString = `mongodb://localhost:27017/${dababaseName}`
+const conetctionString = `mongodb+srv://bodil4o88:8812267960@cluster0.b8lw2kg.mongodb.net/${dababaseName}`
 
 module.exports = async(app) =>{
     try {
