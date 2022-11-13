46ff7f3d226508362854775a1f094940675dcb6b
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2022-11-13 16:36:27 +0200

    corse changes

--- config/express.js
+++ config/express.js
@@ -1,9 +1,10 @@
 const express = require(`express`);
 const auth = require("../middleware/auth");
-const cors = require(`../middleware/cors`);
+// const cors = require(`../middleware/cors`);
+const cors = require('cors')
 
 module.exports = (app) => {
-    app.use(cors());
+    app.use(cors())
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
     app.use(auth());
